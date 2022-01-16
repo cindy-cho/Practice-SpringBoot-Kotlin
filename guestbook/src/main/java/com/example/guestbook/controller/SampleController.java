72597230b29cc4b9d0635115f55ef847466ebcd1
cindy-cho <43ryuflower@gmail.com>
2022-01-16 15:56:49 +0900

    guestbook project update

--- guestbook/src/main/java/com/example/guestbook/controller/SampleController.java
+++ guestbook/src/main/java/com/example/guestbook/controller/SampleController.java
@@ -1,2 +1,16 @@
-package com.example.guestbook.controller;public class SampleController {
+package com.example.guestbook.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sample")
+@Log4j2
+public class SampleController{
+    @GetMapping("/exLayout1")
+    public void exLayout1(){
+        log.info("exLayout..........");
+    }
 }
--- guestbook/src/test/java/com/example/guestbook/repository/GuestbookRepositoryTests.java
+++ guestbook/src/test/java/com/example/guestbook/repository/GuestbookRepositoryTests.java
@@ -1,2 +1,90 @@
-package com.example.guestbook.repository;public class GuestbookRepositoryTests {
+package com.example.guestbook.repository;
+
+import com.example.guestbook.entity.Guestbook;
+import com.example.guestbook.entity.QGuestbook;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class GuestbookRepositoryTests {
+    @Autowired
+    private GuestbookRepository guestbookRepository;
+
+    @Test
+    public void insertDummies(){
+        IntStream.rangeClosed(1,300).forEach(i -> {
+            Guestbook guestbook = Guestbook.builder()
+                    .title("Title...."+i)
+                    .content("Content..."+i)
+                    .writer("user"+(i%10))
+                    .build();
+            System.out.println(guestbookRepository.save(guestbook));
+        });
+    }
+
+    @Test //300번 값 업데이트
+    public void updateTest() {
+        Optional<Guestbook> result = guestbookRepository.findById(300L); //존재하는 번호로 테스트
+
+        if(result.isPresent()){
+            Guestbook guestbook = result.get();
+
+            guestbook.changeTitle("Changed Title...");
+            guestbook.changeContent("Changed Content...");
+
+            guestbookRepository.save(guestbook);
+        }
+    }
+
+    @Test
+    public void testQuery1() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
+
+        QGuestbook qGuestbook = QGuestbook.guestbook; //동적으로 처리하기 위해 Q도메인 클래스를 가져온다
+
+        String keyword = "1";
+
+        BooleanBuilder builder = new BooleanBuilder(); //Where에 들어가는 조건을 넣어준다
+        BooleanExpression expression = qGuestbook.title.contains(keyword); //원하는 조건을 필드값과 결합한다.
+
+        builder.and(expression); //만들어진 조건문을 where의 and, or 같은 키워드와 결합
+
+        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable); //QuerydslPredictateExcutor인터페이스의 findAll() 메소드
+
+        result.stream().forEach(guestbook -> {
+            System.out.println(guestbook);
+        });
+    }
+
+    @Test
+    public void testQuery2() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
+
+        QGuestbook qGuestbook =QGuestbook.guestbook;
+
+        String keyword = "1";
+
+        BooleanBuilder builder = new BooleanBuilder();
+        BooleanExpression exTitle = qGuestbook.title.contains(keyword);
+        BooleanExpression exContent = qGuestbook.content.contains(keyword);
+        BooleanExpression exAll = exTitle.or(exContent);
+        builder.and(exAll);
+        builder.and(qGuestbook.gno.gt(0L));
+
+        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
+
+        result.stream().forEach(guestbook -> {
+            System.out.println(guestbook);
+        });
+    }
 }
