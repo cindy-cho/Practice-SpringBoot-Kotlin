9c86d5c1c4fe06537977236a15ecf7addf773e32
cindy-cho <43ryuflower@gmail.com>
2022-01-18 19:46:53 +0900

    initialize simple library system practicce

---
+++ LibrarySystem/src/main/kotlin/com/practice/librarysystem/LibrarySystemApplication.kt
@@ -0,0 +1,11 @@
+package com.practice.librarysystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LibrarySystemApplication
+
+fun main(args: Array<String>) {
+    runApplication<LibrarySystemApplication>(*args)
+}
---
+++ LibrarySystem/src/test/kotlin/com/practice/librarysystem/LibrarySystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.practice.librarysystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibrarySystemApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
