d46577e1aa6a6362fa151f5a97901610d1a73c0a
cindy-cho <43ryuflower@gmail.com>
2022-01-18 19:48:36 +0900

    add explanation of basic Annotation

--- LibrarySystem/src/main/kotlin/com/practice/librarysystem/LibrarySystemApplication.kt
+++ LibrarySystem/src/main/kotlin/com/practice/librarysystem/LibrarySystemApplication.kt
@@ -3,9 +3,12 @@ package com.practice.librarysystem
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+
+
+@SpringBootApplication  //컴포넌트 검색과 자동구성 활성화
 class LibrarySystemApplication
 
 fun main(args: Array<String>) {
+    // 애플리케이션 부트스트랩
     runApplication<LibrarySystemApplication>(*args)
 }
